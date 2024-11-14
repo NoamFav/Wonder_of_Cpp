48ed593aa07a09ddb4f7eb9c042e2876ad21fdc5
noamfav <noam.favier@icloud.com>
2024-11-14 16:01:53 +0100

    auto-commit

--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 
 class Person {
 public:
-    Person(const std::string& name, int age) : name(name), age(age) {}
+  Person(const std::string &name, int age) : name(name), age(age) {}
 
-    void introduce() const {
-        std::cout << "Hello, my name is " << name << " and I am " << age << " years old." << std::endl;
-    }
+  void introduce() const {
+    std::cout << "Hello, my name is " << name << " and I am " << age
+              << " years old." << std::endl;
+  }
 
 private:
-    std::string name;
-    int age;
+  std::string name;
+  int age;
 };
 
 int main() {
-    Person person("John Doe", 30);
-    person.introduce();
+  Person person("John Doe", 30);
+  person.introduce();
 
-    std::cout << "sizeof(Person) = " << sizeof(Person) << std::endl;
-    std::cout << "cpp is really weird" << std::endl;
+  std::cout << "sizeof(Person) = " << sizeof(Person) << std::endl;
+  std::cout << "cpp is really weird" << std::endl;
 
-    return 0;
+  return 0;
 }
